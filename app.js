a05db6d22bbccd525198911442e0b463c0a38886
Dogukan Ozcan <dogukankerem@hotmail.com>
2022-04-07 19:58:26 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,106 @@
+function Question(text,choices,answer){
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+
+Question.prototype.checkAnswer = function(answer){
+    return this.answer === answer;
+}
+
+
+function Quiz(questions){
+    this.questions = questions;
+    this.score = 0;
+    this.questionIndex = 0
+}
+
+
+Quiz.prototype.getQuestion = function(){
+    return this.questions[this.questionIndex];
+}
+
+
+Quiz.prototype.isFinish = function(){
+    return this.questions.length === this.questionIndex;
+}
+
+// Quiz guess
+Quiz.prototype.guess = function(answer){
+    var question = this.getQuestion();
+
+    if(question.checkAnswer(answer)){
+        this.score++;
+    }
+    this.questionIndex++;
+}
+
+
+var q1 = new Question("What's the best programming language ?",["C#","Javascript","Pyhton","Asp.net"],"Javascript");
+
+var q2 = new Question("What's the most popular programming language ?",["C#","Visual basic","Nodejs","Javascript"],"javascript");
+
+var q3 = new Question("What's the best modern programming language ?",["C#","Javascript","Pyhton","Asp.net"],"Javascript");
+
+var q4 = new Question("what's language ?",["C#","Javascript","Css","Asp.net"],"Javascript");
+
+var q5 = new Question("Modern programming language ?",["C#","Html","Pyhton","Css"],"Javascript");
+
+var questions = [q1,q2,q3,q4,q5];
+
+
+// Start Quiz
+
+var quiz = new Quiz(questions);
+
+loadQuestion();
+
+
+function loadQuestion(){
+    if(quiz.isFinish()){
+        showScore();
+    }else{
+
+        var question = quiz.getQuestion();
+        var choices = question.choices;
+        
+        document.querySelector('#question').textContent = question.text;
+
+        for(var i=0; i<choices.length;i++){
+            var element = document.querySelector('#choice'+i);
+            element.innerHTML = choices[i];
+            guess('btn'+i,choices[i]);
+        }
+
+        showProgress();
+    }
+}
+
+function guess(id,guess){
+    var btn = document.getElementById(id);
+    btn.onclick = function(){
+        quiz.guess(guess);
+        loadQuestion()
+    }
+}
+
+function showScore(){
+   var html = `<h2>Your Score</h2><h4>${quiz.score}</h4>`;
+
+   document.querySelector('.card-body').innerHTML = html;
+}
+
+function showProgress(){
+    var totalQuestion = quiz.questions.length;
+    var questionNumber = quiz.questionIndex+1;
+    var html = 'Question '+ questionNumber + ' of ' + totalQuestion;
+
+    if(totalQuestion === questionNumber){
+        document.querySelector('#progress').innerHTML = "Quiz is Ended";
+    }else{
+        document.querySelector('#progress').innerHTML = html;
+    }
+
+  
+}
